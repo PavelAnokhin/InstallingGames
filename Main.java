f8b84b9fa5360da40a78431f6b17808c9b5a2ea8
PavelAnokhin <pav-anokhin@yandex.ru>
2021-05-24 22:55:12 +0300

    groupping of separatly methods

--- Main.java
+++ Main.java
@@ -1,85 +1,32 @@
 package InstallOfGames;
 
-import com.sun.security.jgss.GSSUtil;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        StringBuilder tempString = new StringBuilder();
-        File src = new File("/Users/pavelanokhin/Документы/Games/src");
-        if(src.mkdir()){
-            tempString.append("src dir was created\n");
-        }
-        File mainDir = new File("/Users/pavelanokhin/Документы/Games/src/Main");
-        if(mainDir.mkdir()){
-            tempString.append("main dir was created\n");
-        }
-        File testDir = new File("/Users/pavelanokhin/Документы/Games/src/test");
-        if(testDir.mkdir()){
-            tempString.append("test dir was created\n");
-        }
-        File mainfile = new File(mainDir, "Main.java");
-        try {
-            if (mainfile.createNewFile()) {
-                tempString.append("Main.java was created in Main dir\n");
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        File utilsfile = new File(mainDir, "Utils.java");
-        try {
-            if (utilsfile.createNewFile()) {
-                tempString.append("Utils.java was created in Main dir\n");
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-        File main = new File("/Users/pavelanokhin/Документы/Games/src/main");
-        if(main.mkdir()){
-            tempString.append("main dir was created\n");
-        }
-        File test = new File("/Users/pavelanokhin/Документы/Games/src/test");
-        if(test.mkdir()){
-            tempString.append("test dir was created\n");
-        }
-        File res = new File("/Users/pavelanokhin/Документы/Games/res");
-        if(res.mkdir()){
-            tempString.append("res dir was created\n");
-        }
-        File drawables = new File( "/Users/pavelanokhin/Документы/Games/res/drawables");
-        if(drawables.mkdir()){
-            tempString.append("drawable dir was created\n");
-        }
-        File vectors = new File("/Users/pavelanokhin/Документы/Games/res/vectors");
-        if(vectors.mkdir()){
-            tempString.append("vectors dir was created\n");
-        }
-        File icons = new File("/Users/pavelanokhin/Документы/Games/res/icons");
-        if(icons.mkdir()){
-            tempString.append("icons dir was created\n");
-        }
-        File savegames = new File("/Users/pavelanokhin/Документы/Games/savegames");
-        if(savegames.mkdir()){
-            tempString.append("savegames dir was created\n");
-        }
-        File temp = new File("/Users/pavelanokhin/Документы/Games/temp");
-        if(temp.mkdir()){
-            tempString.append("temp dir was created\n");
-        }
-        File temptxt = new File(temp, "temp.txt");
-        try {
-            if (temptxt.createNewFile()) {
-                tempString.append("temp.txt was created\n");
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+    private static List<String> tempString = new LinkedList<>();
+
+    public static void main(String[] args)  {
+
+        creatingFolder("/Users/pavelanokhin/Документы/Games/src","src dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/src/Main","main dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/src/test","test dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/res","res dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/res/drawables","drawable dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/res/vectors","vectors dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/res/icons","icons dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/savegames","savegames dir was created");
+        creatingFolder("/Users/pavelanokhin/Документы/Games/temp","temp dir was created");
+        creatingFiles("/Users/pavelanokhin/Документы/Games/src/Main","Main.java");
+        creatingFiles("/Users/pavelanokhin/Документы/Games/src/Main","Utils.java");
+        creatingFiles("/Users/pavelanokhin/Документы/Games/temp","temp.txt");
 
 
-        }
         try {
             FileWriter writer = new FileWriter("/Users/pavelanokhin/Документы/Games/temp/temp.txt", true);
             writer.write(tempString.toString());
@@ -88,6 +35,28 @@ public class Main {
             System.out.println(ext.getMessage());
         }
 
+    }
+
+
+    static void creatingFolder(String adress, String message){
+        File src = new File(adress);
+        if(src.mkdir()){
+            tempString.add(message);
+        }else{
+            System.out.println("Fault during creating folders!Try again");
+        }
+
+    }
+
+    static void creatingFiles(String adress,String nameOfFile){
+        File mainfile = new File(adress, nameOfFile);
+        try {
+            if (mainfile.createNewFile()) {
+                tempString.add(nameOfFile + "was created in" + adress+ "dir\n");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
